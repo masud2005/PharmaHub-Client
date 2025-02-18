3f10dfb8b1985b64967ca7264331acb493b9dafd
masud2005 <masud20012005@gmail.com>
2025-02-19 00:03:17 +0600

    Updated Error Page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 // import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
+import error from '../../assets/404.png'
 
 const ErrorPage = () => {
     return (
         <div className='flex flex-col justify-center items-center min-h-screen'>
 
-            {/* <Helmet>
-                <title>404 Page Not Found | Query Hive</title>
-            </Helmet> */}
+            <Helmet>
+                <title>404 Page Not Found | PharmaHub</title>
+            </Helmet>
 
-            <h1 className='text-9xl font-semibold text-red-500'>404</h1>
-            <h2 className='text-5xl font-medium text-gray-500 py-3'>Page Not Found</h2>
-            <Link to={'/'} className='text-lg text-teal-500 underline'>Go To Home Page</Link>
+            <img className='w-[350px] h-[350px] -mt-10' src={error} alt="Error..." />
+            <Link to={'/'} className='text-xl -mt-10 text-teal-500 font-bold border border-teal-500 rounded-full px-4 py-1 hover:bg-teal-100 transition duration-300'>Go To Home Page</Link>
+
         </div>
     );
 };
